57795fd6419118a9b9e882dc7d0c7464c76bd25c
rodeloescueta <cxcxcxtest@gmail.com>
2020-03-23 15:51:15 +0800

    update pages

--- precache-manifest.c7643b576ee48eb11767e73948f02036.js
+++ precache-manifest.c7643b576ee48eb11767e73948f02036.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/404.html"
   },
   {
-    "revision": "95f9d4fd665d1ce43fc3",
+    "revision": "5de1bd20ff0ca13e508d",
     "url": "/css/app.f55dd7eb.css"
   },
   {
@@ -12,7 +12,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-1d321d30.c7315fda.css"
   },
   {
-    "revision": "71ea46f2fc4d974549b3",
+    "revision": "aedcff9f243dfb23760f",
+    "url": "/css/chunk-2cf92afc.708e88c2.css"
+  },
+  {
+    "revision": "adcfd54e0d85c374335c",
     "url": "/css/chunk-388645bc.c7315fda.css"
   },
   {
@@ -20,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-584ae5f5.10daaa3b.css"
   },
   {
-    "revision": "e5db7e12371819583a03",
-    "url": "/css/chunk-vendors.af4e3907.css"
+    "revision": "3832825c2a9f06eb5f5e",
+    "url": "/css/chunk-vendors.16560689.css"
   },
   {
     "revision": "bd553250d7cbcf393f29a5950ecea31e",
@@ -36,28 +40,28 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/tempPic1.1ca2cb3c.jpg"
   },
   {
-    "revision": "f359c9e27162f3f52b6d1cef90190a1b",
+    "revision": "d6801ca2350a3e547ac9495e2d8d6a1f",
     "url": "/index.html"
   },
   {
-    "revision": "95f9d4fd665d1ce43fc3",
-    "url": "/js/app.9d021128.js"
+    "revision": "5de1bd20ff0ca13e508d",
+    "url": "/js/app.e5497114.js"
   },
   {
     "revision": "adb808d40f866e69949a",
     "url": "/js/chunk-1d321d30.ecb79b63.js"
   },
   {
-    "revision": "71ea46f2fc4d974549b3",
-    "url": "/js/chunk-388645bc.684ae4ca.js"
+    "revision": "5c4d8dd46a3f9c4598fb",
+    "url": "/js/chunk-1e56f00a.34a76bb3.js"
   },
   {
-    "revision": "4d6a385c7c376265321f",
-    "url": "/js/chunk-4afeb879.63689edc.js"
+    "revision": "aedcff9f243dfb23760f",
+    "url": "/js/chunk-2cf92afc.24fc5402.js"
   },
   {
-    "revision": "561707f40a34dd131ba4",
-    "url": "/js/chunk-4b13deec.7132b4bc.js"
+    "revision": "adcfd54e0d85c374335c",
+    "url": "/js/chunk-388645bc.46625af4.js"
   },
   {
     "revision": "3b0317f874d4ef5eaa13",
@@ -68,8 +72,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/chunk-58d06304.897affb6.js"
   },
   {
-    "revision": "e5db7e12371819583a03",
-    "url": "/js/chunk-vendors.fbe91353.js"
+    "revision": "3832825c2a9f06eb5f5e",
+    "url": "/js/chunk-vendors.bccd8ef0.js"
   },
   {
     "revision": "b400fd73d650f2a6a929915212958bf9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8793130dd50e3969cc779d69098df2f9.js"
+  "/precache-manifest.c7643b576ee48eb11767e73948f02036.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "template"});
